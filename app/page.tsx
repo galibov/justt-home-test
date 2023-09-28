a50eff34a8867da3b2b04f15e29404bdedba31b7
Anatoly <galibov@proton.me>
2023-09-29 01:18:25 +0300

    small screen fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
 
     return (
         <div className='flex flex-col justify-center items-center pb-20'>
-            <div className='w-full max-w-screen-2xl my-8'>
+            <div className='w-full max-w-screen-lg 2xl:max-w-screen-2xl my-8'>
                 <Image className='mx-auto' height='200' width='200' src={'/logo.svg'} alt={'logo'}/>
             </div>
-            <div className='flex w-full max-w-screen-2xl mb-10  justify-center'>
+            <div className='flex w-full max-w-screen-lg 2xl:max-w-screen-2xl mb-10  justify-center'>
                 <div className="btn-group">
                     <TabButton title='Browse' isActive={activeTab === 'one'} onClick={() => setActiveTab('one')}/>
                     <TabButton title='Pick a Character' isActive={activeTab === 'two'} onClick={() => setActiveTab('two')}/>
                 </div>
             </div>
-            <div className='w-full max-w-screen-2xl'>
+            <div className='w-full max-w-screen-lg 2xl:max-w-screen-2xl'>
                 {activeTab === 'one' && <BrowseTab />}
                 {activeTab === 'two' && <PickCharacterTab  />}
             </div>
